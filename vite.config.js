4286a19b58fc35e1194db674efcd8f09769871c0
T.H(kaminuma) <moshimo_0817@yahoo.co.jp>
2024-10-19 19:46:48 +0900

    viteConfigの追加

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  define: {
+    'process.env': {
+      BASE_URL: process.env.VITE_BASE_URL, // 環境変数を定義
+    },
+  },
+});
