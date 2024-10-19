02625b7636624038a77628cdb7cf77836fa7a691
T.H(kaminuma) <moshimo_0817@yahoo.co.jp>
2024-10-19 19:48:41 +0900

    Headerの作成

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,26 +1,26 @@
 <template>
-  <header>
-    <h1>{{ title }}</h1>
+  <header class="header">
+    <h1 class="header-title">sbm-application</h1>
   </header>
 </template>
 
 <script>
 export default {
   name: 'Header',
-  props: {
-    title: {
-      type: String,
-      default: 'sbm-application',
-    },
-  },
 };
 </script>
 
 <style scoped>
-header {
-  background-color: #333;
-  color: #fff;
-  text-align: center;
-  padding: 1rem;
+.header {
+  background-color: #001f3f; /* 紺色 */
+  height: 50px; /* 高さ50px */
+  display: flex; /* フレックスボックスで配置 */
+  align-items: center; /* 縦中央揃え */
+  color: white; /* テキストの色を白に */
+  padding: 0 20px; /* 左右にパディングを追加 */
+}
+
+.header-title {
+  margin: 0; /* マージンをリセット */
 }
 </style>
