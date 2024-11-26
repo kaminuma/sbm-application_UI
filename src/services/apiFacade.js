291ee1316eaa9653498f67d79313a93b1f89752a
T.H(kaminuma) <moshimo_0817@yahoo.co.jp>
2024-11-27 00:32:44 +0900

    env修正

--- src/services/apiFacade.js
+++ src/services/apiFacade.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // 環境変数からAPIのベースURLを設定
-const API_BASE_URL = "http://13.211.29.238:8080";
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
 
 // トークンの設定
 const setAuthToken = () => {
